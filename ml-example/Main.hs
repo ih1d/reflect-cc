4a0773ac674373c923bb80d1313ac0a456e161bd
Isaac Lopez <isaac.lopez@upr.edu>
2026-06-12 16:54:01 -0400

    changing to value

--- ml-example/Main.hs
+++ ml-example/Main.hs
@@ -1,31 +1,14 @@
 module Main where
 
+import Control.Monad.Reflection.State (State)
 import Control.Monad.Reflection
-import Control.Monad.Reflection.Except
-import Control.Monad.Reflection.Reader
-import Control.Monad.Reflection.State
-import Data.IORef
-import Control.Monad ((>=>))
 
-newtype Cell a = Cell (IORef a)
+data Value a = Value
+    { val :: a
+    , dval :: a
+    , children :: [a]
+    , op :: Char
+    }
 
-prog hr he hs = do
-    env <- ask hr
-    if env /= "" then get hs >>= \a -> pure (a + 2) else throw he "error"
-    
 main :: IO ()
-main = do
-    mint <- runEff $ runReader "" $ \hr -> runExcept $ \he -> runState (1 :: Int) $ \hs -> prog hr he hs
-    case mint of
-        Left err -> putStrLn err
-        Right int -> print int
-
-main2 :: IO ()
-main2 = do
-    let er = runReader "string" ask
-        ee = runExcept (`throw` "reverse")
-        es = runState (1 :: Int) (get >=> (pure . (+ 2)))
-    rr <- runEff er
-    _ere <- runEff ee
-    rs <- runEff es
-    print (rr, rs) -- cannot print _ere due to being ambiguous
+main = undefined
