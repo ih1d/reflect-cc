d3f8e68c8c9be896e2e2b2bd97409f28d75e8872
Isaac Hiram Lopez Diaz <isaac.lopez@upr.edu>
2026-06-13 21:00:45 -0400

    documentation

--- src/Control/Monad/Reflection/Internal.hs
+++ src/Control/Monad/Reflection/Internal.hs
@@ -7,10 +7,10 @@ import Control.Delimited
 import Data.Kind (Type)
 
 -- | Filinski's T monad
--- 
 type T :: Type -> (Type -> Type) -> Type -> Type
 type family T e m
 
+-- | Eff monad (thin wrapper over IO)
 newtype Eff a = Eff {unEff :: IO a}
     deriving (Functor, Applicative, Monad) via IO
 
@@ -37,8 +37,10 @@ eta emb act = Eff $ do
 mu :: forall e a b. (Monad (T e Eff)) => Handle e b -> T e Eff a -> Eff a
 mu (Handle t (Embed e)) m = Eff (shift t $ \k -> pure (m >>= e . k))
 
+-- | run an IO on Eff
 doIO :: IO a -> Eff a
 doIO = Eff
 
+-- | run an Eff on IO
 runEff :: Eff a -> IO a
 runEff (Eff m) = m
